d37e753dc3889efb79ad1fd6cc8e0dd6e05cf9a3
BlackDChase <blackdragonchase@gmail.com>
2021-02-22 06:25:54 +0530

    Initialised C1

---
+++ course/DataMining/dm.py
@@ -0,0 +1,11 @@
+"""
+Data Mining Matmatical Problem Solver
+- C1
+    - Decision Tree
+    - Apriori Algorithm
+    - Binning
+    - Normalisation
+    - Frequent Pattern Growth Tree
+    - Chi Square Test
+"""
+__version__=='0.0.1'
